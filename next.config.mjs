8d639e321a8ace765740d94a273affa10e89fcb0
shensq03 <siqi_shen03@163.com>
2026-07-06 01:28:11 +0800

    配置GitHub Pages自动部署

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  output: "export",
+  images: { unoptimized: true }
+}
+module.exports = nextConfig
