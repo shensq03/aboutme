eccfff435cc7d40de971463a914acc45ce7f3525
shensq03 <siqi_shen03@163.com>
2026-07-06 01:31:14 +0800

    适配GitHub Pages静态导出

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true }
+  output: "export", // 静态打包适配GitHub Pages
+  images: {
+    unoptimized: true // 解决图片路径404问题
+  }
 }
+
 module.exports = nextConfig
